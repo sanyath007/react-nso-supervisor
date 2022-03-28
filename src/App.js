483f4322d787d56c7e779d9d014519d0d0e81421
sanyath007 <sanyath007@gmail.com>
2022-03-28 14:05:17 +0700

    Create new main components of app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import Daily from './components/Daily';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
 
+      <main>
+        <div className="container">
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/add' element={<Daily />} />
+          </Routes>
+        </div>
+      </main>
+
+      <Footer />
     </div>
   );
 }
