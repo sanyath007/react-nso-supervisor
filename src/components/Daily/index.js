84a96f7d059bcdf40a6fe18cffc498ce27eaefc2
sanyath007 <sanyath007@gmail.com>
2022-03-31 15:30:49 +0700

    Modify Daily by adding new states and function and render data from state

--- src/components/Daily/index.js
+++ src/components/Daily/index.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import FilterTool from './FilterTool';
-import daily from '../../data/daily';
+import api from '../../api';
+import moment from 'moment';
 
 const Daily = () => {
+  const [inspections, setInspections] = useState([]);
+  const [selectedDate, setSelectedDate] = useState(moment().format('YYYY-MM-DD'));
+  const [selectedPeriod, setSelectedPeriod] = useState('1');
+
+  const fetchInspections = async () => {
+    const res = await api.get(`/inspections?date=${selectedDate}&period=${selectedPeriod}`);
+    
+    setInspections(res.data.items);
+  }
+
+  const handleFilter = (date, period) => {
+    console.log(date, period);
+  };
+
+  useEffect(() => {
+    fetchInspections();
+  }, []);
+
   return (
     <>
-      
-      <FilterTool />
+      <FilterTool onSelected={handleFilter} />
 
       <div className="card">
         <div className="card-body">
@@ -49,25 +67,25 @@ const Daily = () => {
               </tr>
             </thead>
             <tbody>
-              {daily && daily.map((dl) => (
-                <tr key={dl.id}>
-                  <td>{dl.name}</td>
-                  <td style={{ textAlign: 'center' }}>{dl.phone}</td>
-                  <td style={{ textAlign: 'center' }}>{dl.beds}</td>
-                  <td style={{ textAlign: 'center' }}>4</td>
-                  <td style={{ textAlign: 'center' }}>5</td>
-                  <td style={{ textAlign: 'center' }}>6</td>
-                  <td style={{ textAlign: 'center' }}>7</td>
-                  <td style={{ textAlign: 'center' }}>8</td>
-                  <td style={{ textAlign: 'center' }}>9</td>
-                  <td style={{ textAlign: 'center' }}>10</td>
-                  <td style={{ textAlign: 'center' }}>11</td>
-                  <td style={{ textAlign: 'center' }}>12</td>
-                  <td style={{ textAlign: 'center' }}>13</td>
-                  <td style={{ textAlign: 'center' }}>14</td>
-                  <td style={{ textAlign: 'center' }}>15</td>
-                  <td style={{ textAlign: 'center' }}>16</td>
-                  <td style={{ textAlign: 'center' }}>17</td>
+              {inspections && inspections.map((inspect) => (
+                <tr key={inspect.id}>
+                  <td>{inspect.office?.name}</td>
+                  <td style={{ textAlign: 'center' }}>{inspect.office?.tel}</td>
+                  <td style={{ textAlign: 'center' }}>{inspect.office?.beds}</td>
+                  <td style={{ textAlign: 'center' }}>{inspect.adds}</td>
+                  <td style={{ textAlign: 'center' }}>{inspect.patient_adult}</td>
+                  <td style={{ textAlign: 'center' }}>{inspect.patient_ped}</td>
+                  <td style={{ textAlign: 'center' }}>{inspect.patient_nicu}</td>
+                  <td style={{ textAlign: 'center' }}>{inspect.patient_snb}</td>
+                  <td style={{ textAlign: 'center' }}>{inspect.patient_nb}</td>
+                  <td style={{ textAlign: 'center' }}>{inspect.patient_total}</td>
+                  <td style={{ textAlign: 'center' }}>{inspect.isolated}</td>
+                  <td style={{ textAlign: 'center' }}>{inspect.officer_rn}</td>
+                  <td style={{ textAlign: 'center' }}>{inspect.officer_emt}</td>
+                  <td style={{ textAlign: 'center' }}>{inspect.officer_pnna}</td>
+                  <td style={{ textAlign: 'center' }}>{inspect.on_birds}</td>
+                  <td style={{ textAlign: 'center' }}>{inspect.on_ventilator}</td>
+                  <td style={{ textAlign: 'center' }}>{inspect.remark}</td>
                   <td style={{ textAlign: 'center' }}>
                     <div className="btn-group">
                       <Link to="/" className="btn btn-warning btn-sm mr-2">
